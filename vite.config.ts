5d74f4d79733b6b9e6b55e0369404595db1b25b8
locatemarketing <ege@locatemarketing.com>
2025-01-10 13:07:44 -0500

    Add Vercel configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,16 @@ export default defineConfig({
     host: '127.0.0.1',
     open: true,
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'chakra': ['@chakra-ui/react', '@emotion/react', '@emotion/styled', 'framer-motion'],
+        }
+      }
+    }
+  }
 }); 
